dc3f63d8f50a472fe4348bfe63378d4894f080f6
Ko-Eunseo <koyk0408@naver.com>
2023-02-08 21:57:37 +0900

    jsdoc으로 todo 설계

--- src/index.js
+++ src/index.js
@@ -1,51 +1,102 @@
 /**
- * Represents a book.
- * @constructor
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
+ * @author Eunseo Go
+ * @see <a href="#">배포링크</a>
  */
-function Book(title, author) {}
 
 /**
- * Both of these will link to the bar function.
- * @see {@link bar}
- * @see bar
+ * Type of tag.
+ * @typedef {Object} TagItem
+ * @property {number} tagId - The id of the tag.
+ * @property {string} tagName = the name of the tag.
  */
-function foo() {}
-
-// Use the inline {@link} tag to include a link within a free-form description.
-/**
- * @see {@link foo} for further information.
- * @see {@link http://github.com|GitHub}
- */
-function bar() {}
 
 /**
- * Generic dairy product.
- * @constructor
+ * Type of tagList.
+ * @type {TagItem[]} TagList
  */
-function DairyProduct() {}
 
 /**
- * Check whether the dairy product is solid at room temperature.
- * @abstract
- * @return {boolean}
+ * Type of todo.
+ * @typedef {Object} TodoItem
+ * @property {number} id - The id of the todo.
+ * @property {string} content - The content of the todo.
+ * @property {boolean} isDone - Whether user did or not todo.
+ * @property {string} category - The category of the todo.
+ * @property {TagList} [tagList] - The tags of the todo.
  */
-DairyProduct.prototype.isSolid = function () {
-	throw new Error('must be implemented by subclass!');
-};
 
 /**
- * Cool, refreshing milk.
- * @constructor
- * @augments DairyProduct
+ * Type of todoList.
+ * @type {TodoItem[]} TodoList
  */
-function Milk() {}
 
 /**
- * Check whether milk is solid at room temperature.
- * @return {boolean} Always returns false.
+ * @function Create todo.
+ * @param {string} content 
+ * @param {string?} category 
+ * @param {string[]?} tags 
+ * @return {TodoItem}
+ * @see {@link TodoItem}
  */
-Milk.prototype.isSolid = function () {
-	return false;
-};
+const CreateTodo = (content, category, tag) => { };
+
+/**
+ * @function Read all of todos.
+ * @return {TodoList}
+ * @see {@link TodoList}
+ */
+const ReadAllTodo = () => { };
+
+/**
+ * @function Read specific todo by id.
+ * @param {number} id
+ * @return {TodoItem}
+ * @see {@link TodoItem}
+ */
+const ReadTodoById = (id) => { };
+
+/**
+ * Update todo.
+ * @param {number} id - The id of the todo.
+ * @param {string} [content] - The content of the todo.
+ * @param {string} [category] - The category of the todo.
+ * @return {TodoItem} - return updated todoItem.
+ */
+const UpdateTodo = (id, content, category) => { };
+
+/**
+ * Update specific todo's specific tag.
+ * @param {number} id - The id of the todo.
+ * @param {number} [tagId] - The tagId of the todo.
+ * @param {string} [tagName] - The tag's name of the todo.
+ * @return {TodoItem} - return updated todoItem.
+ */
+const UpdateTag = (id, tagId, tagName) => { };
+
+/**
+ * @function Delete todo by id.
+ * @param {number} id - The id of the todo.
+ * @return {void}
+ */
+const DeleteTodoById = (id) => { };
+
+/**
+ * @function Delete all todo.
+ * @return {void}
+ */
+const DeleteAllTodo = () => { };
+
+/**
+ * @function Delete tag by id.
+ * @param {string} id - The id of the todo.
+ * @param {string} tagId - The id of the tag.
+ * @return {TodoItem} - return updated todoItem.
+ */
+const DeleteTagById = (id, tagId) => { };
+
+/**
+ * @function Delete all tag.
+ * @param {number} id - The id of the todo.
+ * @return {TodoItem} - return updated todoItem.
+ */
+const DeleteAllTag = () => { };
